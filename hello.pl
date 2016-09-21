2ed0aa0b41103b8e9c593af1bc02977c131c544f
EricKeep <ek349112@ohio.edu>
2016-09-21 15:57:58 -0400

    modified the hello world file

--- hello.pl
+++ hello.pl
@@ -3,4 +3,34 @@
 use strict;
 use warnings;
 
+# Test for printing
 print "Hello World!\n";
+
+# Creates a simple test string
+my $test_str = "This is a test string\n";
+
+# Print out the test string
+print $test_str;
+
+# Split string into elements of an array
+my @test_str_arr = split ' ', $test_str;
+
+#Test printing as an array
+
+foreach(@test_str_arr){
+	print($_);
+	print(" ");
+}
+print "[as an array]\n";
+
+#Test adding elements to an array
+
+push (@test_str_arr, "split");
+push (@test_str_arr, "up");
+
+foreach(@test_str_arr){
+	print($_);
+	print(" ");
+}
+
+print "\n";
