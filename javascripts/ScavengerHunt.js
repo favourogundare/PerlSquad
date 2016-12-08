6a52420e6f24983901c3bd7f0b728dde46102294
unknown <brianjreynolds@hotmail.com>
2016-12-07 23:02:41 -0500

    Removed console.logs() and added in jsdoc comments

--- javascripts/ScavengerHunt.js
+++ javascripts/ScavengerHunt.js
@@ -1,3 +1,7 @@
+/**
+ *@function eventScavengerHunt
+ *Item gathering portion of game.
+ */
 function eventScavengerHunt() {
     var BOX_WIDTH  = 760;
     var BOX_HEIGHT = 244;
@@ -32,6 +36,10 @@ function eventScavengerHunt() {
     var minSort;
     var color;
     
+    /**
+     *@function this.onAnimal
+     *Extended function that handles when scavHuntAnimal container is clicked by highlighting "Animal" button.
+     */
     this.onAnimal =  function(event){
         selected = 1;
         outcircle1.graphics.clear().beginFill("#616161").drawCircle(1 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
@@ -42,6 +50,10 @@ function eventScavengerHunt() {
         game.getStage().update();
     }
     
+    /**
+     *@function this.onGrowingSeason
+     *Extended function that handles when scavHuntGrowingSeason container is clicked by highlighting "Growing Season" button.
+     */
     this.onGrowingSeason = function(event){
         selected = 2;
         outcircle1.graphics.clear().beginFill("#bdbdbd").drawCircle(1 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
@@ -52,6 +64,10 @@ function eventScavengerHunt() {
         game.getStage().update();
     }
     
+    /**
+     *@function this.onPlantLife
+     *Extended function that handles when scavHuntPlantLife container is clicked by highlighting "Plant Life" button.
+     */
     this.onPlantLife = function(event){
         selected = 3;
         outcircle1.graphics.clear().beginFill("#bdbdbd").drawCircle(1 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
@@ -62,6 +78,10 @@ function eventScavengerHunt() {
         game.getStage().update();
     }
     
+    /**
+     *@function this.onPrecipitation
+     *Extended function that handles when scavHuntPrecipitation container is clicked by highlighting "Precipitation" button.
+     */
     this.onPrecipitation = function(event){
         selected = 4;
         outcircle1.graphics.clear().beginFill("#bdbdbd").drawCircle(1 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
@@ -72,6 +92,11 @@ function eventScavengerHunt() {
         game.getStage().update();
     }
     
+    
+    /**
+     *@function this.onLatitude
+     *Extended function that handles when scavHuntLatitude container is clicked by highlighting "Latitude" button.
+     */
     this.onLatitude = function(event){
         selected = 5;
         outcircle1.graphics.clear().beginFill("#bdbdbd").drawCircle(1 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
@@ -82,6 +107,11 @@ function eventScavengerHunt() {
         game.getStage().update();
     }
     
+    
+    /**
+     *@function this.onOK2
+     *Extended function that handles when Item Info OK2 is clicked by moving the default highlighted/selected option to the first button still available if there are remaining items or by removing the infoContainer and calling game.progress().
+     */
     this.onOK2 = function(event){
         if (remaining){
             game.getStage().removeChild(infoContainer);
@@ -89,12 +119,9 @@ function eventScavengerHunt() {
             minSort = selectFrom.concat().sort();
             color = "#616161";
             selected = minSort[0];
-            console.log("selected = ", selected);
             for (i = 0; i < remaining; i++){
-                console.log(minSort[i]);
                 switch(minSort[i]) {
                     case 1:
-                        console.log("check");
                         outcircle1.graphics.clear().beginFill(color).drawCircle(1 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
                         break;
                     case 2:
@@ -121,7 +148,11 @@ function eventScavengerHunt() {
             game.progress();
         }
     }
-		
+	
+    /**
+     *@function this.onOK
+     *Extended function that handles when scavenger hunt OK is clicked by displaying the corresponding item info and removing the selected item from available options.
+     */	
     this.onOK = function(event) {
         infoContainer = new createjs.Container();
         infoContainer.x = 100;
@@ -161,15 +192,23 @@ function eventScavengerHunt() {
         game.getStage().update();
     }
 		
-    // Public eventListener to handle double clicking on the player
+    /**
+     *@function this.handleDblClick
+     *Extended function that handles double clicking on the player by proceeding with the scavenger hunt portion of the game.
+     */
     this.handleDblClick = function(event) {
-        // Delete old text
+        /**Delete old text*/
         game.getMainContainer().removeChild(text)
+        
+        /*scavHunt container section*/
         scavHuntContainer = new createjs.Container();
         scavHuntContainer.x = 100;
         scavHuntContainer.y = 100;
+        
+        /**background for scavHunt container section*/
         scavHuntBackground = new createjs.Shape();
         scavHuntBackground.graphics.beginFill("#000000").drawRect(0, 0, BOX_WIDTH, BOX_HEIGHT);
+        
         /*var scavHuntAnimalPic = new createjs.Bitmap("images/tiger.png");
         scavHuntAnimalPic.x = 50;
         scavHuntAnimalPic.y = 25;
@@ -182,12 +221,14 @@ function eventScavengerHunt() {
         scavHuntText.y = 122;
         scavHuntContainer.addChild(scavHuntText);*/
         
+        /**initialize button containers*/
         scavHuntAnimal        = new createjs.Container();
         scavHuntGrowingSeason = new createjs.Container();
         scavHuntPlantLife     = new createjs.Container();
         scavHuntPrecipitation = new createjs.Container();
         scavHuntLatitude      = new createjs.Container();
         
+        /**initialize button shapes*/
         outcircle1    = new createjs.Shape();
         outcircle2    = new createjs.Shape();
         outcircle3    = new createjs.Shape();
@@ -199,9 +240,10 @@ function eventScavengerHunt() {
         incircle4     = new createjs.Shape();
         incircle5     = new createjs.Shape();
         
+        /**initial default selection*/
         selected = 1;
         
-        //animal section
+        /**animal button container section*/
         outcircle1.graphics.beginFill("#616161").drawCircle(1 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
         incircle1.graphics.beginFill("#f44336").drawCircle(1 * BOX_WIDTH/6, BOX_HEIGHT/3, 40);
         scavHuntAnimalText = new createjs.Text("Animal", "20px Arial", "#FFFFFF");
@@ -213,7 +255,7 @@ function eventScavengerHunt() {
         scavHuntAnimal.addEventListener("click", this.onAnimal);
         scavHuntAnimal.addChildAt(outcircle1, incircle1, scavHuntAnimalText, 0);
         
-        //growing season section
+        /**growing season button container section*/
         outcircle2.graphics.beginFill("#bdbdbd").drawCircle(2 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
         incircle2.graphics.beginFill("#ff9100").drawCircle(2 * BOX_WIDTH/6, BOX_HEIGHT/3, 40);
         scavHuntGrowingSeasonText = new createjs.Text("Growing Season", "20px Arial", "#FFFFFF");
@@ -225,7 +267,7 @@ function eventScavengerHunt() {
         scavHuntGrowingSeason.addEventListener("click", this.onGrowingSeason);
         scavHuntGrowingSeason.addChildAt(outcircle2, incircle2, scavHuntGrowingSeasonText, 0);
         
-        //plant life section
+        /**plant life button container section*/
         outcircle3.graphics.beginFill("#bdbdbd").drawCircle(3 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
         incircle3.graphics.beginFill("#00e676").drawCircle(3 * BOX_WIDTH/6, BOX_HEIGHT/3, 40);
         scavHuntPlantLifeText = new createjs.Text("Plant Life", "20px Arial", "#FFFFFF");
@@ -237,7 +279,7 @@ function eventScavengerHunt() {
         scavHuntPlantLife.addEventListener("click", this.onPlantLife);
         scavHuntPlantLife.addChildAt(outcircle3, incircle3, scavHuntPlantLifeText, 0);
         
-        //precipitation section
+        /**precipitation button container section*/
         outcircle4.graphics.beginFill("#bdbdbd").drawCircle(4 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
         incircle4.graphics.beginFill("#00b0ff").drawCircle(4 * BOX_WIDTH/6, BOX_HEIGHT/3, 40);
         scavHuntPrecipitationText = new createjs.Text("Precipitation", "20px Arial", "#FFFFFF");
@@ -249,7 +291,7 @@ function eventScavengerHunt() {
         scavHuntPrecipitation.addEventListener("click", this.onPrecipitation);
         scavHuntPrecipitation.addChildAt(outcircle4, incircle4, scavHuntPrecipitationText, 0);
         
-        //latitude section
+        /**latitude button container section*/
         outcircle5.graphics.beginFill("#bdbdbd").drawCircle(5 * BOX_WIDTH/6, BOX_HEIGHT/3, 50);
         incircle5.graphics.beginFill("#d500f9").drawCircle(5 * BOX_WIDTH/6, BOX_HEIGHT/3, 40);
         scavHuntLatitudeText = new createjs.Text("Latitude", "20px Arial", "#FFFFFF");
@@ -261,18 +303,19 @@ function eventScavengerHunt() {
         scavHuntLatitude.addChildAt(outcircle5, incircle5, scavHuntLatitudeText, 0);
         scavHuntLatitude.addEventListener("click", this.onLatitude);
         
-        //OK section
+        /**OK button section*/
         scavHuntOK = new createjs.Text("OK", "20px Arial", "#FFFFFF");
         scavHuntOK.x = 9 * BOX_WIDTH / 10;
         scavHuntOK.y = 4 * BOX_HEIGHT / 5;
         scavHuntOK.addEventListener("click", this.onOK);
         
+        /**Build and update stage*/
         scavHuntContainer.addChildAt(scavHuntBackground, scavHuntAnimal, scavHuntGrowingSeason, scavHuntPlantLife, scavHuntPrecipitation, scavHuntLatitude, scavHuntOK, 0);
         game.getStage().addChild(scavHuntContainer);
         game.getStage().update();
     }
 
-    // What will be executed when the GameEvent function pointer is triggered
+    /**What will be executed when the GameEvent function pointer is triggered*/
     var text = new createjs.Text("Double Click Now!", "20px Arial", "#ff7700");
     text.x = 350;
     text.y = 350;
