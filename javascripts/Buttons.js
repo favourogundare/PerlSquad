41377018f8d121706d1875dcf104bd04b54a3eec
Sagerune <fo893113@ohio.edu>
2017-02-27 17:33:50 -0500

    changed text boxes, and functions

--- javascripts/Buttons.js
+++ javascripts/Buttons.js
@@ -23,7 +23,61 @@ function RectButton(buttonText, buttonColor, buttonX, buttonY, buttonW, buttonH,
 	this.outRect.graphics.beginFill(outColor).drawRect(0, 0, buttonW, buttonH);
 	this.inRect           = new createjs.Shape();		
 	this.inRect.graphics.beginFill(buttonColor).drawRect(10, 10, buttonW - 20, buttonH - 20);
-	this.container.addChildAt(this.outRect, this.inRect, this.txt, 0);
+	this.container.addChild(this.outRect, this.inRect, this.txt);
+	this.container.addEventListener(buttonEvent, buttonFunction);
+} 
+
+/**
+ *  @function RectButton
+ *  @param buttonText
+ *  @param buttonColor
+ *  @param buttonX
+ *  @param buttonY
+ *  @param buttonW
+ *  @param buttonH
+ *  @param buttonEvent
+ *  @param buttonFunction
+ *  Rectangular button class.
+ */
+function HelpButton1(buttonText, buttonTextFormat, buttonTextColor, buttonColor, buttonX, buttonY, buttonW, buttonH, buttonEvent, buttonFunction) {
+	this.container        = new createjs.Container();
+	this.container.x      = buttonX;
+	this.container.y      = buttonY;
+	this.txt   		      = new createjs.Text(buttonText, buttonTextFormat, buttonTextColor);
+	this.txt.textAlign    = "center";
+	this.txt.textBaseline = "middle";
+	this.txt.x 	          = buttonW/2;
+	this.txt.y            = 10 + buttonH/3;
+	this.Rect             = new createjs.Shape();		
+	this.Rect.graphics.beginFill(buttonColor).drawRect(0, 0, buttonW, buttonH);
+	this.container.addChild(this.Rect, this.txt);
+	this.container.addEventListener(buttonEvent, buttonFunction);
+} 
+
+/**
+ *  @function RectButton
+ *  @param buttonText
+ *  @param buttonColor
+ *  @param buttonX
+ *  @param buttonY
+ *  @param buttonW
+ *  @param buttonH
+ *  @param buttonEvent
+ *  @param buttonFunction
+ *  Rectangular button class.
+ */
+function HelpButton2(buttonText, buttonTextFormat, buttonTextColor, buttonTextX, buttonTextY, buttonColor, buttonX, buttonY, buttonW, buttonH, buttonEvent, buttonFunction) {
+	this.container        = new createjs.Container();
+	this.container.x      = buttonX;
+	this.container.y      = buttonY;
+	this.txt   		      = new createjs.Text(buttonText, buttonTextFormat, buttonTextColor);
+	this.txt.textAlign    = "center";
+	this.txt.textBaseline = "middle";
+	this.txt.x 	          = buttonTextX;
+	this.txt.y            = buttonTextY;
+	this.Rect             = new createjs.Shape();		
+	this.Rect.graphics.beginFill(buttonColor).drawRect(0, 0, buttonW, buttonH);
+	this.container.addChild(this.Rect, this.txt);
 	this.container.addEventListener(buttonEvent, buttonFunction);
 } 
 
@@ -56,6 +110,6 @@ function CircleButton(buttonText, buttonTextFormat, buttonTextX, buttonTextY, bu
 	this.outCircle.graphics.beginFill(outColor).drawCircle(0, 0, buttonR);
 	this.inCircle           = new createjs.Shape();		
 	this.inCircle.graphics.beginFill(buttonColor).drawCircle(0, 0, buttonR - 10);
-	this.container.addChildAt(this.outCircle, this.inCircle, this.txt, 0);
+	this.container.addChild(this.outCircle, this.inCircle, this.txt);
 	this.container.addEventListener(buttonEvent, buttonFunction);
 }
--- javascripts/HelpPage.js
+++ javascripts/HelpPage.js
@@ -21,30 +21,37 @@ function eventHelpPage() {
 	var help_container_background = new createjs.Shape();
 	help_container_background.graphics.beginFill("#000000").drawRect(game.getStage().width/2 - 300, game.getStage().height/2 - 150, 600, 300);
 
-	var HelpPageText = new createjs.Text("FAQ", "36px Times New Roman", "#ffffff");
+	var HelpPageText = new createjs.Text("FAQ : CHOOSE A TOPIC", "36px Arial", "#ffffff");
 	HelpPageText.x = game.getStage().width/2;
 	HelpPageText.y = 110;
 	HelpPageText.textAlign = "center";
 	HelpPageText.textBaseline = "middle";
 
-	help_container.addChild(help_container_background, HelpPageText);
+	var help_addimage_button  = new RectButton("Add Image", "#000000", game.getStage().width/2 - 150, 150, 300, 50, "#D3D3D3", "click", addimageClicked);
+	var help_editimage_button   = new RectButton("Edit Image", "#000000", game.getStage().width/2 - 150, 225, 300, 50, "#D3D3D3", "click", editimageClicked);
+	var help_gamemech_button   = new RectButton("Game Mechanics", "#000000", game.getStage().width/2 - 150, 300, 300, 50, "#D3D3D3", "click", gamemechClicked);
+/*
+	var help_addimage_button  = new HelpButton1(buttonText, "20px Arial", buttonTextColor, buttonColor, buttonX, buttonY, buttonW, buttonH, buttonEvent, buttonFunction);
+	var help_editimage_button  = new HelpButton1(buttonText, buttonTextFormat, buttonTextColor, buttonColor, buttonX, buttonY, buttonW, buttonH, buttonEvent, buttonFunction);
+	var help_gamemech_button  = new HelpButton1(buttonText, buttonTextFormat, buttonTextColor, buttonColor, buttonX, buttonY, buttonW, buttonH, buttonEvent, buttonFunction);
+*/
+
+	help_container.addChild(help_container_background, HelpPageText, help_addimage_button.container, help_editimage_button.container, help_gamemech_button.container);
 	game.getStage().addChild(help_container);
 	game.getStage().update();
-
-
-
-
-
-
-
-	/*
-    "use strict";
 	
-	*/
+function addimageClicked(){
+		game.getStage().update();
+	}
+	
+	function editimageClicked(){
+		game.getStage().update();
+	}
+	
+	function gamemechClicked(){
+		game.getStage().update();
+	}
 
-	//var addimage_button  = new RectButton("Add Image", "#00e676", game.getStage().width/2 - 150, 150, 300, 50, "#212121", "click", addimageClicked);
-	//var editimage_button   = new RectButton("Edit Image", "#ff9100", game.getStage().width/2 - 150, 225, 300, 50, "#212121", "click", editimageClicked);
-	//var gamemech_button   = new RectButton("Game Mechanics", "#f44336", game.getStage().width/2 - 150, 300, 300, 50, "#616161", "click", gamemechClicked);
 
 }
 
