8a50f9477aab90c91366f1f368294edfba6fec17
Sagerune <fo893113@ohio.edu>
2017-02-27 18:35:44 -0500

    added functions

--- javascripts/HelpPage.js
+++ javascripts/HelpPage.js
@@ -18,30 +18,47 @@ function eventHelpPage() {
         onOk - remove container from stage and call eventStartMenu
     */
 	var help_container = new createjs.Container();
-	var help_container_background = new createjs.Shape();
-	help_container_background.graphics.beginFill("#000000").drawRect(game.getStage().width/2 - 300, game.getStage().height/2 - 150, 600, 300);
+	/*var help_container_background = new createjs.Shape();
+	help_container_background.graphics.beginFill("#D3D3D3").drawRect(game.getStage().width/2 - 300, game.getStage().height/2 - 150, 600, 300);*/
 
-	var HelpPageText = new createjs.Text("FAQ : CHOOSE A TOPIC", "36px Arial", "#ffffff");
+	var HelpPageText = new createjs.Text("FAQ : CHOOSE A TOPIC", "36px Arial", "#000000");
 	HelpPageText.x = game.getStage().width/2;
 	HelpPageText.y = 110;
 	HelpPageText.textAlign = "center";
 	HelpPageText.textBaseline = "middle";
-
+/*
 	var help_addimage_button  = new RectButton("Add Image", "#000000", game.getStage().width/2 - 150, 150, 300, 50, "#D3D3D3", "click", addimageClicked);
 	var help_editimage_button   = new RectButton("Edit Image", "#000000", game.getStage().width/2 - 150, 225, 300, 50, "#D3D3D3", "click", editimageClicked);
 	var help_gamemech_button   = new RectButton("Game Mechanics", "#000000", game.getStage().width/2 - 150, 300, 300, 50, "#D3D3D3", "click", gamemechClicked);
-/*
-	var help_addimage_button  = new HelpButton1(buttonText, "20px Arial", buttonTextColor, buttonColor, buttonX, buttonY, buttonW, buttonH, buttonEvent, buttonFunction);
-	var help_editimage_button  = new HelpButton1(buttonText, buttonTextFormat, buttonTextColor, buttonColor, buttonX, buttonY, buttonW, buttonH, buttonEvent, buttonFunction);
-	var help_gamemech_button  = new HelpButton1(buttonText, buttonTextFormat, buttonTextColor, buttonColor, buttonX, buttonY, buttonW, buttonH, buttonEvent, buttonFunction);
 */
+	var help_addimage_button  = new HelpButton1("Add Image", "20px Arial", "#000000", "#A9A9A9", game.getStage().width/2 - 150, 150, 300, 50, "click", addimageClicked);
+	var help_editimage_button = new HelpButton1("Edit Image", "20px Arial", "#000000", "#A9A9A9", game.getStage().width/2 - 150, 225, 300, 50, "click", editimageClicked);
+	var help_gamemech_button  = new HelpButton1("Game Mechanics", "20px Arial", "#000000", "#A9A9A9", game.getStage().width/2 - 150, 300, 300, 50, "click", gamemechClicked);
 
-	help_container.addChild(help_container_background, HelpPageText, help_addimage_button.container, help_editimage_button.container, help_gamemech_button.container);
+
+	help_container.addChild(HelpPageText, help_addimage_button.container, help_editimage_button.container, help_gamemech_button.container);
 	game.getStage().addChild(help_container);
 	game.getStage().update();
 	
-function addimageClicked(){
+	function addimageClicked(){
+		var help_subcontainer = new createjs.Container();
+		help_subcontainer.on("dblclick", function(event) {
+			help_container.removeChild(help_subcontainer);
+			game.getStage().update();
+		});
+		var help_subcontainer_background = new createjs.Shape();
+		help_subcontainer_background.graphics.beginFill("#D3D3D3").drawRect(game.getStage().width/2 - 300, game.getStage().height/2 - 150, 600, 300);
+
+		var HelpSubPageText = new createjs.Text("BLAH BLAH ADD AN IMAGE TEXT", "24px Arial", "#000000");
+		HelpSubPageText.x = game.getStage().width/2;
+		HelpSubPageText.y = 110;
+		HelpSubPageText.textAlign = "center";
+		HelpSubPageText.textBaseline = "middle";
+
+		help_subcontainer.addChild(help_subcontainer_background, HelpSubPageText);
+		help_container.addChild(help_subcontainer);
 		game.getStage().update();
+
 	}
 	
 	function editimageClicked(){
